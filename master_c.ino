ad642205dd526dd1307c18a00fed34e19e57a91e
works-as-programmer <worksasprogrammer@gmail.com>
2025-02-13 11:21:46 +0530

    Update master_c.ino

--- master_c.ino
+++ master_c.ino
@@ -17,23 +17,32 @@ void setup() {
     esp_now_peer_info_t peerInfo;
     memset(&peerInfo, 0, sizeof(peerInfo));
 
+    // Add Left Slave
     memcpy(peerInfo.peer_addr, leftSlave, 6);
     peerInfo.channel = 0;
     peerInfo.encrypt = false;
     esp_now_add_peer(&peerInfo);
 
+    // Add Right Slave
     memcpy(peerInfo.peer_addr, rightSlave, 6);
     esp_now_add_peer(&peerInfo);
+
+    Serial.println("Type '1' to send to Left, '2' to send to Right.");
 }
 
 void loop() {
-    const char *message = "Vibrate!";
-    
-    esp_err_t result = esp_now_send(leftSlave, (uint8_t *)message, strlen(message));
-    Serial.println(result == ESP_OK ? "Sent to Left" : "Failed to send Left");
+    if (Serial.available()) {
+        char input = Serial.read();
+        const char *message = "Vibrate!";
+        esp_err_t result;
 
-    result = esp_now_send(rightSlave, (uint8_t *)message, strlen(message));
-    Serial.println(result == ESP_OK ? "Sent to Right" : "Failed to send Right");
-
-    delay(100);  // Faster transmission
+        if (input == '1') {
+            result = esp_now_send(leftSlave, (uint8_t *)message, strlen(message));
+            Serial.println(result == ESP_OK ? "Sent to Left" : "Failed to send Left");
+        } 
+        else if (input == '2') {
+            result = esp_now_send(rightSlave, (uint8_t *)message, strlen(message));
+            Serial.println(result == ESP_OK ? "Sent to Right" : "Failed to send Right");
+        }
+    }
 }
